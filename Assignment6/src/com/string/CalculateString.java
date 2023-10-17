c6db9ddf3ab29ae86818576c3eeda8e60e11052c
Anita-parmar <parmaranu111@gmail.com>
2023-10-17 17:07:33 +0530

    Assignment06

---
+++ Assignment6/src/com/string/CalculateString.java
@@ -0,0 +1,32 @@
+package com.string;
+
+import java.util.Scanner;
+
+public class CalculateString {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a string");
+		String   s= sc.nextLine();
+		System.out.println(s);
+		try {
+		if(s.length()>8)
+			throw new ExceptionLineTooLong();
+			System.out.println("Length of string "+s+"="+s.length());
+		}catch(ExceptionLineTooLong e) {
+		System.out.println("The strings is too long");
+		}
+		
+//		if(s.length()>8)
+//			throw new ExceptionLineTooLong();
+
+
+
+	}
+
+}
+
+
+	
+
---
+++ Assignment6/src/com/string/ExceptionLineTooLong.java
@@ -0,0 +1,10 @@
+package com.string;
+
+public class ExceptionLineTooLong extends Exception {
+
+	public ExceptionLineTooLong() {
+		//System.out.println("The strings is too long");
+
+	}
+
+}
